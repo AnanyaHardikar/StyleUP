39fbb338ef9d3f02b27601cf55083103af34eaf8
AnanyaHardikar <ananya.hardikar@gmail.com>
2020-11-25 19:34:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+app.secret_key="secret key"
+
+@app.route('/')
+def index():
+   return render_template('home.html')
+
+@app.route('/blog/')
+def blog():
+   return render_template('blog.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
