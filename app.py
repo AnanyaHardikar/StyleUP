11390a016a04646c0252d0ed3d35d11b9e15ff9f
Amitha18 <amithalokeshwar18@gmail.com>
2020-11-27 11:25:40 +0530

    men page added,related images inserted.

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ app.secret_key="secret key"
 @app.route('/')
 def index():
    return render_template('home.html')
-
+@app.route('/home/')
+def home():
+    return render_template('home.html')
 @app.route('/blog/')
 def blog():
-   return render_template('blog.html')
-
-
+    return render_template('blog.html')
+@app.route('/men/')
+def men():
+    return render_template('men.html')
 if __name__ == "__main__":
     app.run(debug=True)
